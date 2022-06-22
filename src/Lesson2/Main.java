804b44a36f7598ac7c1ffd4f852d39aa20693d67
asblood3000 <asblood3000@gmail.com>
2022-06-22 18:53:23 +0300

    just commit

--- src/Lesson2/Main.java
+++ src/Lesson2/Main.java
@@ -1,7 +1,25 @@
 package Lesson2;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
+        String[][] myArray1 = {{"1", "2", "3", "4"}, {"7", "6", "7", "8"}, {"9", "ш", "7", "8"}, {"5", "6", "7", "8"}};
+        String[][] myArray2 = {{"1", "2", "3", "4"}, {"7", "6", "7", "8"}, {"9", "5", "7", "8"}};
+        String[][] myArray3 = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "6", "7", "8"}, {"5", "6", "7", "8"}};
 
+        CheckException arrays = new CheckException();
+        arrays.arrayChecking(myArray3);
+
+        try {
+            arrays.arrayChecking(myArray2);
+        } catch (MyArraySizeException ex) {
+            System.out.println(ex.getMessage() + " " + myArray2.length);
+        }
+
+        try {
+            arrays.arrayChecking(myArray1);
+        } catch (MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+
