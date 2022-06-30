cbb65df55069a01a55ff4ea9f56fac90580b57e7
asblood3000 <asblood3000@gmail.com>
2022-06-30 19:54:56 +0300

    just commit

--- src/Lesson3/task1/ChangeArray.java
+++ src/Lesson3/task1/ChangeArray.java
@@ -3,11 +3,11 @@ package Lesson3.task1;
 import java.util.Arrays;
 
 public class ChangeArray {
-    public static void change(Object[] arr, int n1, int n2){
-        System.out.println("Исходное состояние массива: " + Arrays.toString(arr));
-        Object change = arr[n1];
+    public static void change (Object[] arr, int n1, int n2){
+        System.out.println("Исходное состояние массива: "+Arrays.toString(arr));
+        Object sw = arr[n1];
         arr[n1]=arr[n2];
-        arr[n2]=change;
-        System.out.println("Результат замены элемента " + n1 + " на элемент " + n2 + ": " + Arrays.toString(arr));
+        arr[n2]=sw;
+        System.out.println("Результат замены элемента массива " + n1 + " на элемент " + n2 + ": " + Arrays.toString(arr));
     }
 }
--- src/Lesson3/task1/Main.java
+++ src/Lesson3/task1/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Integer arr1[] = {1, 2, 3, 4};
         String arr2[] = {"А", "Б", "В"} ;
-        ChangeArray.change(arr1,1,3);
+        ChangeArray.change(arr1,0,3);
         ChangeArray.change(arr2,1,2);
     }
 }
