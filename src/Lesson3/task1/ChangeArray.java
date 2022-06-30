14e8661b222ea69366c889d2ba4ebd1ac0819fce
asblood3000 <asblood3000@gmail.com>
2022-06-30 19:56:11 +0300

    just commit

--- src/Lesson3/task1/ChangeArray.java
+++ src/Lesson3/task1/ChangeArray.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class ChangeArray {
     public static void change (Object[] arr, int n1, int n2){
-        System.out.println("Исходное состояние массива: "+Arrays.toString(arr));
-        Object sw = arr[n1];
+        System.out.println("Исходное состояние массива: " + Arrays.toString(arr));
+        Object a = arr[n1];
         arr[n1]=arr[n2];
-        arr[n2]=sw;
+        arr[n2]=a;
         System.out.println("Результат замены элемента массива " + n1 + " на элемент " + n2 + ": " + Arrays.toString(arr));
     }
 }
