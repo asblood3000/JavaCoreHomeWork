204912ec7bf71135a7364b6b44a05085aad04187
asblood3000 <asblood3000@gmail.com>
2022-08-18 00:14:32 +0300

    just commit

---
+++ src/Lesson5/CreateFileClass.java
@@ -0,0 +1,34 @@
+package Lesson5;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class CreateFileClass {
+    public static ArrayList<FIleObject> fIleObjectArrayList = new ArrayList<>();
+    public static final String pathToFile = "C://new/demo.csv";
+    public static final String title = "value1"+ ";" + "value2"
+            + ";" + "value3" + ";"+ System.getProperty("line.separator");
+
+    public static void main(String[] args) throws IOException {
+        createFileObjects();
+        writer();
+    }
+
+    public static void createFileObjects(){
+        Random random = new Random();
+        for(int i=0;i<20;i++){
+            fIleObjectArrayList.add( new FIleObject(random.nextInt(100), random.nextInt(100), random.nextInt(100)));
+        }
+    }
+
+    public static void writer() throws IOException {
+        try (FileWriter writer = new FileWriter(pathToFile);){
+            writer.write(title);
+            for(FIleObject fIleObject : fIleObjectArrayList) {
+                writer.write(fIleObject.getValue1() + ";" + fIleObject.getValue2()
+                        + ";" + fIleObject.getValue3() + ";"+ System.getProperty("line.separator"));
+            }
+        }
+    }
+}
---
+++ src/Lesson5/FIleObject.java
@@ -0,0 +1,25 @@
+package Lesson5;
+
+public class FIleObject {
+    private Integer value1;
+    private Integer value2;
+    private Integer value3;
+
+    public FIleObject(Integer value1, Integer value2, Integer value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public Integer getValue1() {
+        return value1;
+    }
+
+    public Integer getValue2() {
+        return value2;
+    }
+
+    public Integer getValue3() {
+        return value3;
+    }
+}
