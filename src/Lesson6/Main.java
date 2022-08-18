1b2cb8bd2a276f5de2a47faf21d0e6215cc41cdc
asblood3000 <asblood3000@gmail.com>
2022-08-19 01:18:08 +0300

    just commit

--- src/Lesson6/Main.java
+++ src/Lesson6/Main.java
@@ -1,7 +1,10 @@
 package Lesson6;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
 
     }
 }
