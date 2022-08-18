4ac8f220ee05fa6c13d50362ca19c3b2a87a8d8f
asblood3000 <asblood3000@gmail.com>
2022-08-19 00:42:02 +0300

    just commit

--- src/Lesson5/AppData.java
+++ src/Lesson5/AppData.java
@@ -7,16 +7,8 @@ public class AppData {
     private String[] header;
     private int[][] data;
 
-    public String[] getHeader() {
-        return header;
-    }
-
     public void setHeader(String[] header) {
         this.header = header;
-    }
-
-    public int[][] getData() {
-        return data;
     }
 
     public void setData(int[][] data) {
